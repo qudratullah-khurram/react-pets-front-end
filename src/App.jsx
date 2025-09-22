eb3a12bc5a90897166d1f3924f2e1732bdb2ca56
Qudratullah Khurram <qudrat.khurram@gmail.com>
2025-09-22 22:15:14 +0100

    completed: pet details, pet list and pet form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,66 @@
-// src/App.jsx
+import { useState, useEffect } from 'react';
+import * as petService from './services/petService';
+import PetList from './components/PetList/PetList';
+import PetDetail from './components/PetDetail/PetDetail';
+import PetForm from './components/PetForm/PetForm';
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [pets, setPets] = useState([]);
+  const [selected, setSelected] = useState (null);
+  const [isFormOpen, setIsFormOpen] = useState(false);
+
+  // Create a new useEffect
+  useEffect(() => {
+    // Create a new async function
+    const fetchPets = async () => {
+      try {
+      // Call on the pet service's index function
+      const fetchedPets = await petService.index();
+      if (fetchedPets.err) {
+        throw new Error(fetchedPets.err)
+      }
+      // Set pets state to the returned pets data
+       setPets(fetchedPets);
+      } catch (err) {
+        // Log the error object
+        console.log(err);
+      }
+    };
+    fetchPets();
+  }, []);
+
+const handleSelect = (pet) => {
+  setSelected (pet)
+}
+ const handleFormView = () => {
+    setIsFormOpen(!isFormOpen);
+  };
+
+  
+  const handleAddPet = async (formData) => {
+    try {
+  
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <>
+      <PetList
+        pets={pets}
+        handleSelect={handleSelect}
+        handleFormView={handleFormView}
+        isFormOpen={isFormOpen}
+      />
+       {isFormOpen ? (
+        <PetForm handleAddPet={handleAddPet} />
+      ) : (
+        <PetDetail selected={selected} />
+      )}
+    </>
+  );
+
+
 };
 
 export default App;
---
+++ src/components/PetList/PetList.jsx
@@ -0,0 +1,31 @@
+// src/components/PetList/PetList.jsx
+
+const PetList = (props) => {
+  // Let's ensure we have data to work with before adding functionality!
+  console.log(props);
+
+  return (
+    <div>
+      <h1>Pet List</h1>
+      <div>
+        {!props.pets.length ? (
+          <h2>No Pets Yet!</h2>
+        ) : (
+          <ul>
+            {props.pets.map((pet) => (
+               <li 
+              key={pet._id}
+              style={{ cursor: 'pointer', color: "#646CFF" }}
+             onClick={() => props.handleSelect(pet)}>  {pet.name}
+            </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    <button onClick={props.handleFormView}>
+        {props.isFormOpen ? 'Close Form' : 'New Pet'}
+      </button>
+    </div>
+  );
+}
+export default PetList;
